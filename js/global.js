8d824ef464805e0a2ccd0022eb9f1efee0b85f1b
rebehayan <rebehayan@gmail.com>
2023-11-17 16:36:08 +0900

    FE Design System

--- js/global.js
+++ js/global.js
@@ -1,3 +1,12 @@
 import { SideNav } from "./ui/SideNav.js";
+import { Toggle } from "./ui/Toggle.js";
 
-SideNav();
+//방법1
+// SideNav();
+
+// 방법2
+Toggle({
+  button: ".btn-menu",
+  target: ".side-nav",
+  targetClass: "--active",
+});
--- js/ui/Toggle.js
+++ js/ui/Toggle.js
@@ -2,15 +2,17 @@ export function Toggle(option = {}) {
   const config = {
     button: "button",
     target: "div",
-    targetClass: "active",
+    targetClass: "--active",
     ...option,
   };
   const $toggleBtn = document.querySelector(config.button);
   const $toggleTarget = document.querySelector(config.target);
 
-  $toggleBtn.addEventListener("click", openNav);
+  $toggleBtn.addEventListener("click", openTarget);
 
-  function openNav() {
+  console.log(config.targetClass);
+
+  function openTarget() {
     if ($toggleTarget.classList.contains(config.targetClass)) {
       $toggleTarget.classList.remove(config.targetClass);
     } else {
