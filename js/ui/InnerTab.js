8886e81920f3f2d42b1c1f7774e8895f017c93d0
rebehayan <rebehayan@gmail.com>
2023-11-21 19:26:36 +0900

    FE Design System

--- js/ui/InnerTab.js
+++ js/ui/InnerTab.js
@@ -6,7 +6,8 @@ export function InnerTab(option = {}) {
   const $button = document.querySelectorAll(".tab-type1 a");
 
   function init() {
-    bindEvent();
+    // bindEvent();
+    scrollMagicTab();
   }
 
   function bindEvent() {
@@ -26,6 +27,13 @@ export function InnerTab(option = {}) {
     $button.forEach(function (el) {
       el.classList.remove(config.activeClass);
     });
+  }
+  function scrollMagicTab() {
+    var controller = new ScrollMagic.Controller({ globalSceneOptions: { duration: 1000 } });
+    new ScrollMagic.Scene({ triggerElement: "#content1" }).setClassToggle("#tab1-1", "--active").addTo(controller);
+    new ScrollMagic.Scene({ triggerElement: "#content2" }).setClassToggle("#tab1-2", "--active").addTo(controller);
+    new ScrollMagic.Scene({ triggerElement: "#content3" }).setClassToggle("#tab1-3", "--active").addTo(controller);
+    new ScrollMagic.Scene({ triggerElement: "#content4" }).setClassToggle("#tab1-4", "--active").addTo(controller);
   }
 
   init();
